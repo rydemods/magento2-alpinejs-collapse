528d9e4fc49bc3042fba370b0d2a28b2f94f9533
Stevie <mods2003@gmail.com>
2024-01-31 13:22:24 +0900

    change to collapse

--- src/registration.php
+++ src/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FriendsOfHyva_AlpineJsPersist', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Swd_AlpineJsCollapse', __DIR__);
